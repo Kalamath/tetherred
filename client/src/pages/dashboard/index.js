97d62ef5d6aec6f93476e898f1137acca192b13b
syeda zaidi <zaidisyeda03@gmail.com>
2019-11-06 13:13:10 -0800

    dashboard page

---
+++ client/src/pages/dashboard/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { useAuth } from "../../utils/customHooks";
+
+function Dashboard() {
+  const [userState] = useAuth();
+  if (!userState.isLoggedIn) {
+    return (
+      <div className="container">
+        <div className="alert alert-danger">
+          You must be logged in to view this page
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="container">
+      <div className="row">
+        <h1>Dashboard</h1>
+      </div>
+      <div className="row">
+        <p>I am an amazing dashboard</p>
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
