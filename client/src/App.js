b7f777f895ef03ab768ab937224e1ba34c494998
Kalamath <thecodedimage@gmail.com>
2019-11-06 16:28:12 -0800

    finished basic wireframe

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,48 @@
 import React from 'react';
-// import Books from "./pages/Books";
 import Nav from "./components/Nav";
-import Jumbotron from "./components/Jumbotron";
+import Jumbotron from 'react-bootstrap/Jumbotron'
 import { Container } from "./components/Grid";
 import Home from "./pages/Home"
-import React, { useEffect } from 'react';
-import {
-  BrowserRouter as Router,
-  Switch, 
-  Route
-} from 'react-router-dom';
-import axios from 'axios';
-import Header from './components/Header';
-import Homepage from './pages/Homepage';
-import Dashboard from './pages/Dashboard';
-import Login from './pages/Login';
-import Logout from './pages/Logout';
-import Signup from './pages/signup';
-import {useAuth} from './utils/customHooks';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import React, { useEffect } from 'react';
+// import {
+//   BrowserRouter as Router,
+//   Switch, 
+//   Route
+// } from 'react-router-dom';
+// import axios from 'axios';
+// import Header from './components/Header';
+// import Homepage from './pages/Homepage';
+// import Dashboard from './pages/Dashboard';
+// import Login from './pages/Login';
+// import Logout from './pages/Logout';
+// import Signup from './pages/signup';
+// import {useAuth} from './utils/customHooks';
 
 function App() {
 
-  const [, setUser] = useAuth();
+  // const [, setUser] = useAuth();
 
-  const runOnce = true;
-  useEffect(() => {
-        axios
-        .get("/api/sessions/start", {
-          withCredentials: true
-        })
-        .then(resp => {
-          if (resp.data.isLoggedIn) {
-            setUser({ isLoggedIn: true });
-          } else {
-            setUser({ isLoggedIn: false });
-          }
-        })
-        .catch(err => {
-          console.log(err);
-        });
-  }, 
-  // By passing an empty array of dependencies as the second parameter, this ensures that `useEffect` only runs once
-  [runOnce]); 
+  // const runOnce = true;
+  // useEffect(() => {
+  //       axios
+  //       .get("/api/sessions/start", {
+  //         withCredentials: true
+  //       })
+  //       .then(resp => {
+  //         if (resp.data.isLoggedIn) {
+  //           setUser({ isLoggedIn: true });
+  //         } else {
+  //           setUser({ isLoggedIn: false });
+  //         }
+  //       })
+  //       .catch(err => {
+  //         console.log(err);
+  //       });
+  // }, 
+  // // By passing an empty array of dependencies as the second parameter, this ensures that `useEffect` only runs once
+  // [runOnce]); 
   
 
   return (
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,125 +1,142 @@
 import React, { Component } from "react";
-import Jumbotron from "../../components/Jumbotron";
-import DeleteBtn from "../../components/DeleteBtn";
-import API from "../../utils/API";
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+// import DeleteBtn from "../../components/DeleteBtn";
+// import API from "../../utils/API";
 import { Col, Row, Container } from "../../components/Grid";
-import { List, ListItem } from "../../components/List";
-import { Input, TextArea, FormBtn } from "../../components/Form";
+// import { List, ListItem } from "../../components/List";
+// import { Input, TextArea, FormBtn } from "../../components/Form";
 
 class Home extends Component {
-  // Setting our component's initial state
-  state = {
-    books: [],
-    title: "",
-    author: "",
-    synopsis: ""
-  };
+    // Setting our component's initial state
+    state = {
+        books: [],
+        title: "",
+        author: "",
+        synopsis: ""
+    };
 
-  // When the component mounts, load all books and save them to this.state.books
-  componentDidMount() {
-    this.loadBooks();
-  }
+    // When the component mounts, load all books and save them to this.state.books
+    componentDidMount() {
+        this.loadBooks();
+    }
 
-  // Loads all books  and sets them to this.state.books
-  loadBooks = () => {
-    // API.getBooks()
-    //   .then(res =>
-    //     this.setState({ books: res.data, title: "", author: "", synopsis: "" })
-    //   )
-    //   .catch(err => console.log(err));
-  };
+    // Loads all books  and sets them to this.state.books
+    loadBooks = () => {
+        // API.getBooks()
+        //   .then(res =>
+        //     this.setState({ books: res.data, title: "", author: "", synopsis: "" })
+        //   )
+        //   .catch(err => console.log(err));
+    };
 
-  // Deletes a book from the database with a given id, then reloads books from the db
-  deleteBook = id => {
-    // API.deleteBook(id)
-    //   .then(res => this.loadBooks())
-    //   .catch(err => console.log(err));
-  };
+    // Deletes a book from the database with a given id, then reloads books from the db
+    deleteBook = id => {
+        // API.deleteBook(id)
+        //   .then(res => this.loadBooks())
+        //   .catch(err => console.log(err));
+    };
 
-  // Handles updating component state when the user types into the input field
-  handleInputChange = event => {
-    // const { name, value } = event.target;
-    // this.setState({
-    //   [name]: value
-    // });
-  };
+    // Handles updating component state when the user types into the input field
+    handleInputChange = event => {
+        // const { name, value } = event.target;
+        // this.setState({
+        //   [name]: value
+        // });
+    };
 
-  // When the form is submitted, use the API.saveBook method to save the book data
-  // Then reload books from the database
-  handleFormSubmit = event => {
-    // event.preventDefault();
-    // if (this.state.title && this.state.author) {
-    //   API.saveBook({
-    //     title: this.state.title,
-    //     author: this.state.author,
-    //     synopsis: this.state.synopsis
-    //   })
-    //     .then(res => this.loadBooks())
-    //     .catch(err => console.log(err));
-    // }
-  };
+    // When the form is submitted, use the API.saveBook method to save the book data
+    // Then reload books from the database
+    handleFormSubmit = event => {
+        // event.preventDefault();
+        // if (this.state.title && this.state.author) {
+        //   API.saveBook({
+        //     title: this.state.title,
+        //     author: this.state.author,
+        //     synopsis: this.state.synopsis
+        //   })
+        //     .then(res => this.loadBooks())
+        //     .catch(err => console.log(err));
+        // }
+    };
 
-  render() {
-    return (
-      <Container fluid>
-        <Row>
-        <Col size="md-3 sm-12">
-            <Jumbotron>
-              <h1>Options List</h1>
-            </Jumbotron>
-        
-          </Col>
-          <Col size="md-6" style="position: absolute; overflow-y: scroll;">
-            <Jumbotron>
-              <h1>User Content</h1>
-            </Jumbotron>
-            <Jumbotron>
-              <h1>User Content</h1>
-            </Jumbotron>
-            <Jumbotron>
-              <h1>User Content</h1>
-            </Jumbotron>
-            <Jumbotron>
-              <h1>User Content</h1>
-            </Jumbotron>
-            <Jumbotron>
-              <h1>User Content</h1>
-            </Jumbotron>
-            <Jumbotron>
-              <h1>User Content</h1>
-            </Jumbotron>
-            <Jumbotron>
-              <h1>User Content</h1>
-            </Jumbotron>
-            <Jumbotron>
-              <h1>User Content</h1>
-            </Jumbotron>
-            <Jumbotron>
-              <h1>User Content</h1>
-            </Jumbotron>
-          </Col>
-          <Col size="md-3">
-           <Jumbotron>
-            <form>
-              <TextArea
-                value={this.state.synopsis}
-                onChange={this.handleInputChange}
-                name="synopsis"
-                placeholder="What's Going On?"
-              />
-              <FormBtn
-                disabled={!(this.state.author && this.state.title)}
-                onClick={this.handleFormSubmit}
-              >
-                Chirp
-              </FormBtn>
-            </form> 
-            </Jumbotron>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
+    render() {
+        return (
+            <Container fluid>
+                <Row>
+                    <Col size="md-3 sm-12">
+                        <Form>
+                            {['checkbox'].map(type => (
+                                <div key={type} className="mb-3">
+                                    <Form.Check type={type} id={`check-api-${type}`}>
+                                        <Form.Check.Input type={type} isValid />
+                                        <Form.Check.Label>{`Photos`}</Form.Check.Label>
+                                        {/* <Form.Control.Feedback type="valid">You did it!</Form.Control.Feedback> */}
+                                    </Form.Check>
+                                </div>
+                            ))}
+                            {['checkbox'].map(type => (
+                                <div key={type} className="mb-3">
+                                    <Form.Check type={type} id={`check-api-${type}`}>
+                                        <Form.Check.Input type={type} isValid />
+                                        <Form.Check.Label>{`News`}</Form.Check.Label>
+                                        {/* <Form.Control.Feedback type="valid">You did it!</Form.Control.Feedback> */}
+                                    </Form.Check>
+                                </div>
+                            ))}
+                            {['checkbox'].map(type => (
+                                <div key={type} className="mb-3">
+                                    <Form.Check type={type} id={`check-api-${type}`}>
+                                        <Form.Check.Input type={type} isValid />
+                                        <Form.Check.Label>{`Family & Friends`}</Form.Check.Label>
+                                        {/* <Form.Control.Feedback type="valid">You did it!</Form.Control.Feedback> */}
+                                    </Form.Check>
+                                </div>
+                            ))}
+                        </Form>
+                    </Col>
+                    <Col size="md-6" style="position: absolute; overflow-y: scroll;">
+                        <Jumbotron>
+                            <Container>
+                                <Card>
+                                    <Card.Img variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTojRfbevBO4zr6_0sB3-4OtqRHW7nbpVDcYXIPIA67IY6NRVza" />
+                                    <Card.Body>
+                                        <Card.Title>Card Title</Card.Title>
+                                        <Card.Text>
+                                            Some quick example text to build on the card title and make up the bulk of
+                                            the card's content.
+                                </Card.Text>
+                                        <Button variant="primary">Go somewhere</Button>
+                                    </Card.Body>
+                                </Card>
+                            </Container>
+                        </Jumbotron>
+                        <Jumbotron />
+                        <Jumbotron />
+                        <Jumbotron />
+                        <Jumbotron />
+                        <Jumbotron />
+                        <Jumbotron />
+                        <Jumbotron />
+                    </Col>
+                    <Col size="md-3">
+                        <Jumbotron>
+                            <Form>
+                                <Form.Group controlId="exampleForm.ControlTextarea1">
+                                    <Form.Label>Chirp Box</Form.Label>
+                                    <Form.Control as="textarea" rows="3" />
+                                </Form.Group>
+                            </Form>
+                        </Jumbotron>
+                        <Jumbotron />
+                        <Jumbotron />
+                    </Col>
+                </Row>
+            </Container>
+        );
+    }
 }
 
 export default Home;
