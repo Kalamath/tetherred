245da8789f28b7ae0a10b907e182f0ecf1003d1f
Kalamath <thecodedimage@gmail.com>
2019-11-07 17:09:10 -0800

    testing the login

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useEffect } from 'react';
 import {
   BrowserRouter as Router,
-  Switch, 
+  Switch,
   Route
 } from 'react-router-dom';
 import axios from 'axios';
@@ -18,8 +18,8 @@ import Homepage from './pages/Homepage';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 import Logout from './pages/Logout';
-import Signup from './pages/signup';
-import {useAuth} from './utils/customHooks';
+import Signup from './pages/Signup';
+import { useAuth } from './utils/customHooks';
 
 function App() {
 
@@ -27,35 +27,35 @@ function App() {
 
   const runOnce = true;
   useEffect(() => {
-        axios
-        .get("/api/sessions/start", {
-          withCredentials: true
-        })
-        .then(resp => {
-          if (resp.data.isLoggedIn) {
-            setUser({ isLoggedIn: true });
-          } else {
-            setUser({ isLoggedIn: false });
-          }
-        })
-        .catch(err => {
-          console.log(err);
-        });
-  }, 
-  // By passing an empty array of dependencies as the second parameter, this ensures that `useEffect` only runs once
-  [runOnce]); 
+    axios
+      .get("/api/sessions/start", {
+        withCredentials: true
+      })
+      .then(resp => {
+        if (resp.data.isLoggedIn) {
+          setUser({ isLoggedIn: true });
+        } else {
+          setUser({ isLoggedIn: false });
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  },
+    // By passing an empty array of dependencies as the second parameter, this ensures that `useEffect` only runs once
+    [runOnce]);
 
 
   return (
     <Router>
-        <Header />
-        <Switch>
-          <Route path="/" exact={true} component={Homepage} />
-          <Route path="/dashboard" exact={true} component={Dashboard} />
-          <Route path="/signin" exact={true} component={Login} />
-          <Route path="/signup" exact={true} component={Signup} />
-          <Route path="/logout" exact={true} component={Logout} />
-        </Switch>
+      <Header />
+      <Switch>
+        <Route path="/" exact={true} component={Homepage} />
+        <Route path="/dashboard" exact={true} component={Dashboard} />
+        <Route path="/signin" exact={true} component={Login} />
+        <Route path="/signup" exact={true} component={Signup} />
+        <Route path="/logout" exact={true} component={Logout} />
+      </Switch>
     </Router>
   );
 }
