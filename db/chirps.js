3dd177fdbcdbcf02c0edc671a6aad591ead925a5
syeda zaidi <zaidisyeda03@gmail.com>
2019-11-07 17:27:50 -0800

    chirp model

---
+++ db/chirps.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const ChirpsSchema = new mongoose.Schema({
+
+    author: {
+        type: String,
+        required: true
+    },
+
+    chirpBody: {
+        type: String,
+        required: true
+    },
+
+    date: {
+        type: Date,
+        default: Date.now
+    },
+
+});
+
+const ChirpsModel = mongoose.model("Chirps", ChirpsSchema);
+
+module.exports = ChirpsModel;
