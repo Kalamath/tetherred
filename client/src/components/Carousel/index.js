b12cf69efdc9fd1ecb0594aa0c868015117b7d36
Kalamath <thecodedimage@gmail.com>
2019-11-08 21:47:15 -0800

    UserProfile

--- client/src/components/Carousel/index.js
+++ client/src/components/Carousel/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Carousel from 'react-bootstrap/Carousel'
-import Container from 'react-bootstrap/Container'
+// import Container from 'react-bootstrap/Container'
 import Jumbotron from 'react-bootstrap/Jumbotron'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
