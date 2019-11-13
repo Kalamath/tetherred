ec86fe19c27d35a9c795e49f2b5a3f962b4244b7
syeda zaidi <zaidisyeda03@gmail.com>
2019-11-12 16:52:55 -0800

    making db commection in index

--- db/index.js
+++ db/index.js
@@ -1,4 +1,21 @@
-module.exports = {
-    User: require("./user"),
-    Chirps: require("./chirps")
-}
+//Connect to Mongo database
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+//your local database url
+//27017 is the default mongoDB port
+const localURI = 'mongodb://localhost:27017/tetherredDB' 
+
+mongoose.connect(process.env.MONGODB_URI || localURI, {useNewUrlParser: true})
+.then( () => { 
+
+    console.log('Connected to Mongo');
+        
+    }).catch ( err => {
+       
+         console.log('error connecting to Mongo: ')
+         console.log(err);
+         
+        });
+  
+module.exports = mongoose.connection
