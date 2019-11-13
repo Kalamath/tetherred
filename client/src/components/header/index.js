52a46ddab5533bb6adac53c65985fa4e687e563e
syeda zaidi <zaidisyeda03@gmail.com>
2019-11-12 17:39:35 -0800

    changed into class comp

--- client/src/components/header/index.js
+++ client/src/components/header/index.js
@@ -1,72 +1,55 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useAuth } from "../../utils/customHooks";
+// import { useAuth } from "../../utils/customHooks";
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 import logo from '../../assets/imgs/TetherredFullBlack.png'
 
-function Header() {
-  const [state] = useAuth();
-  return (
-    <Navbar>
-      <Navbar.Brand href="#home">
-      <img
-        alt=""
-        src={logo}
-        width="300"
-        height="62.5"
-        className="d-inline-block align-top"
-      />
-    </Navbar.Brand>
-      <Navbar.Toggle />
-      <Navbar.Collapse className="justify-content-end">
-        <Nav className="justify-content-end">
-          <Nav.Link href="/dashboard">Home</Nav.Link>
-          <Nav.Link href="/profile">Profile</Nav.Link>
-          <Nav.Link href="/signup">Sign Up</Nav.Link>
-          <Nav.Link href="/logout">Logout</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+class Header extends React.Component {
 
-    // <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-    //   <Link className="navbar-brand" to="/">
-    //     React Sessions Demo
-    //   </Link>
-    //   <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    //     <ul className="navbar-nav mr-auto">
-    //       {/* <li className="nav-item active">
-    //         <Link className="nav-link" to="/">
-    //           Home
-    //         </Link>
-    //       </li> */}
-    //       <li className="nav-item">
-    //         {state.isLoggedIn ? (
-    //           <Link className="nav-link" to="/dashboard">
-    //             Dashboard
-    //           </Link>
-    //         ) : (
-    //             <div>
-    //               <Link className="nav-link" to="/">
-    //                 Login
-    //             </Link>
-    //               <Link className="nav-link" to="/signup">
-    //                 SignUp
-    //             </Link>
-    //             </div>
-    //           )}
-    //       </li>
-    //       {state.isLoggedIn ? (
-    //         <li>
-    //           <Link className="nav-link" to="/logout">
-    //             Logout
-    //           </Link>
-    //         </li>
-    //       ) : null}
-    //     </ul>
-    //   </div>
-    // </nav>
-  );
+
+  render() {
+
+    const loggedIn = this.props.loggedIn;
+    console.log('header render, props: ')
+    console.log(this.props);
+
+    return (
+      <Navbar>
+        <Navbar.Brand href="#home">
+          <img
+            alt=""
+            src={logo}
+            width="300"
+            height="62.5"
+            className="d-inline-block align-top"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Nav className="justify-content-end">
+            {loggedIn ? (
+
+              <div>
+                <Nav.Link href="/dashboard">Home</Nav.Link>
+                <Nav.Link href="/profile">Profile</Nav.Link>
+                <Nav.Link href="/logout">Logout</Nav.Link>
+              </div>
+
+            ) : (
+
+                <div>
+                  <Nav.Link href="/signin">login</Nav.Link>
+                  <Nav.Link href="/signup">Sign Up</Nav.Link>
+                </div>
+                
+              )}
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+
+    );
+  }
 }
 export default Header;
