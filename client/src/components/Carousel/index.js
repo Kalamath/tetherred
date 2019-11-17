87e5ef74501be55b691a1b63b52df26009dd327b
Kalamath <thecodedimage@gmail.com>
2019-11-17 15:13:09 -0800

    Shrunk the carousel

--- client/src/components/Carousel/index.js
+++ client/src/components/Carousel/index.js
@@ -4,59 +4,65 @@ import Carousel from 'react-bootstrap/Carousel'
 import Jumbotron from 'react-bootstrap/Jumbotron'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Container from 'react-bootstrap/Container'
 
-import axios from "axios"; 
+import axios from "axios";
 function makeRequest() {
   return axios.get('/api/news')
-  
+
 }
 function TopCarousel() {
-  const [articles, setData] = useState([]); 
+  const [articles, setData] = useState([]);
   // async await is the same as .then but prettier
   // should wrap inside try catch 
   // to prevent errors and you need a catch
   // async MUST appear before callback function
-  useEffect( () => {
-    
-      (async function() {
+  useEffect(() => {
 
-        try {
+    (async function () {
 
-          const response = await makeRequest();
-          console.log(response);
-          // not sure why but we had to create collection of components or we got
-          // stupid error...
-          const componentCollection = response.data.map((item, index) => {
-            return ( 
-              <Carousel.Item key={index}>
-                <img src={item.image} />
-                <Carousel.Caption>
-                  <p>{item.title}</p>
-                </Carousel.Caption>
-              </Carousel.Item>
-            )
-          });
-          setData(componentCollection);
-          console.log('balfldsjfljsk', componentCollection);
-        } catch(e) {
-          console.log(e);
-        } 
+      try {
 
-      })();
+        const response = await makeRequest();
+        console.log(response);
+        // not sure why but we had to create collection of components or we got
+        // stupid error...
+        const componentCollection = response.data.map((item, index) => {
+          return (
+            <Carousel.Item key={index}>
+              <img
+                className="d-inline-block w-100"
+                src={item.image}
+                alt=""
+              />
+              <Carousel.Caption>
+                <p>{item.title}</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          )
+        });
+        setData(componentCollection);
+        console.log('balfldsjfljsk', componentCollection);
+      } catch (e) {
+        console.log(e);
+      }
+
+    })();
 
 
   }, []);
 
   return (
-   <Jumbotron>
-        
+    <Container>
+      <Jumbotron>
         <Carousel>
-          
+
           {articles}
-          
+
         </Carousel>
-   
-     </Jumbotron> 
+
+      </Jumbotron>
+    </Container>
   );
 }
 
