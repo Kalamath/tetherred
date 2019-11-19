8bf15c0870374439b033e4d17f3224240c687c8e
Kalamath <thecodedimage@gmail.com>
2019-11-18 20:50:31 -0800

    Martell's Commit

---
+++ client/src/components/NewsBox/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+// import { Redirect } from './node_modules/react-router-dom';
+// import axios from "axios";
+
+import GridItem from "../Grid/GridItem.js";
+// import Container from './node_modules/react-bootstrap/Container';
+// import GridContainer from "../Grid/GridContainer.js";
+// import Button from "../CustomButtons/Button.js";
+import Card from "../Card/Card.js";
+import CardBody from "../Card/CardBody.js";
+import CardHeader from "../Card/CardHeader.js";
+import NewsCarousel from "../../components/NewsCarousel";
+// import TextField from '@material-ui/core/TextField';
+
+
+export default function NewsBox() {
+
+    return (
+        <GridItem xs={12} sm={12} md={4}>
+            <Card className="bg-transparent" style={{ width: "100%" }}>
+            <CardHeader color="danger"><h4>News</h4></CardHeader>
+                <CardBody>
+                   <NewsCarousel />
+                </CardBody>
+            </Card>
+        </GridItem>
+    );
+}
+
