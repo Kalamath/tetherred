dcc180f03d351a0e1cfe89529cf74468254b06e5
Kalamath <thecodedimage@gmail.com>
2019-11-18 20:59:48 -0800

    news div

--- client/src/components/EntertainmentBox/index.js
+++ client/src/components/EntertainmentBox/index.js
@@ -5,35 +5,22 @@ import React from "react";
 import GridItem from "../Grid/GridItem.js";
 // import Container from './node_modules/react-bootstrap/Container';
 // import GridContainer from "../Grid/GridContainer.js";
-import Button from "../CustomButtons/Button.js";
+// import Button from "../CustomButtons/Button.js";
 import Card from "../Card/Card.js";
 import CardBody from "../Card/CardBody.js";
 import CardHeader from "../Card/CardHeader.js";
-import TextField from '@material-ui/core/TextField';
+import NewsCarousel from "../../components/NewsCarousel";
+// import TextField from '@material-ui/core/TextField';
 
 
 export default function EntertainmentBox() {
 
     return (
         <GridItem xs={12} sm={12} md={4}>
-            <Card style={{ width: "100%" }}>
-            <CardHeader color="danger"><h4>Entertainment</h4></CardHeader>
+            <Card className="bg-dark" style={{ width: "100%" }}>
+            <CardHeader color="danger"><h4>News</h4></CardHeader>
                 <CardBody>
-                    <TextField
-                        id="outlined-multiline-static"
-                        label="Write Chirp Here"
-                        // defaultValue={this.state.postChirp}
-                        multiline
-                        rows="5"
-                        style={{ width: "100%" }}
-                        // className={classes.textField}
-                        margin="normal"
-                        variant="outlined"
-                        name="chirpbox"
-                    />
-                    <Button color="info">
-                        See More
-                        </Button>
+                   <NewsCarousel />
                 </CardBody>
             </Card>
         </GridItem>
